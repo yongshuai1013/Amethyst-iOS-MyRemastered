8b027de286db37f1802c0c733f31233a024f01a0
yitenchen123 <2916782430@qq.com>
2026-02-15 17:00:50 +0800

    更新 ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -1,12 +1,15 @@
 #import <Foundation/Foundation.h>
 #import "ModpackAPI.h"
 #import "ModVersion.h"
+#import "ShaderVersion.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ModrinthAPI : ModpackAPI
 + (instancetype)sharedInstance;
 - (void)getVersionsForModWithID:(NSString *)modID completion:(void (^)(NSArray<ModVersion *> * _Nullable versions, NSError * _Nullable error))completion;
+- (void)searchShaderWithFilters:(NSDictionary *)filters completion:(void (^)(NSArray * _Nullable results, NSError * _Nullable error))completion;
+- (void)getVersionsForShaderWithID:(NSString *)shaderID completion:(void (^)(NSArray<ShaderVersion *> * _Nullable versions, NSError * _Nullable error))completion;
 @end
 
 NS_ASSUME_NONNULL_END

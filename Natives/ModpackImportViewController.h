5f20cf708ed1a40aa5ec99d74fe2beda7bbecba5
yitenchen123 <2916782430@qq.com>
2026-02-17 15:57:53 +0800

    创建 ModpackImportViewController.h

---
+++ Natives/ModpackImportViewController.h
@@ -0,0 +1,18 @@
+//
+//  ModpackImportViewController.h
+//  Amethyst
+//
+//  整合包导入视图控制器
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModpackImportViewController : UIViewController
+
+@property (nonatomic, copy, nullable) NSString *profileName;
+
+@end
+
+NS_ASSUME_NONNULL_END

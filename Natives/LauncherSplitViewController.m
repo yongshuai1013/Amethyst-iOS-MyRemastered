fecbad9462f896c9fedc8bf84ef3738302fca309
yitenchen123 <2916782430@qq.com>
2026-02-21 16:47:00 +0800

    更新 LauncherSplitViewController.m

--- Natives/LauncherSplitViewController.m
+++ Natives/LauncherSplitViewController.m
@@ -9,15 +9,17 @@
 
 extern NSMutableDictionary *prefDict;
 
-@interface LauncherSplitViewController ()<UISplitViewControllerDelegate>{
-}
+@interface LauncherSplitViewController ()<UISplitViewControllerDelegate>
 @end
 
 @implementation LauncherSplitViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = UIColor.systemBackgroundColor;
+    
+    // Set transparent background to show global background
+    self.view.backgroundColor = [UIColor clearColor];
+    
     if ([getPrefObject(@"control.control_safe_area") length] == 0) {
         setPrefObject(@"control.control_safe_area", NSStringFromUIEdgeInsets(getDefaultSafeArea()));
     }
@@ -33,13 +35,19 @@ extern NSMutableDictionary *prefDict;
     
     self.maximumPrimaryColumnWidth = self.view.bounds.size.width * 0.95;
     
-    // Apply background wallpaper
-    [self applyBackground];
+    // Apply global background to this split view controller
+    [[BackgroundManager sharedManager] applyBackgroundToSplitViewController:self];
     
     // Listen for background change notifications
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(backgroundChanged:)
                                                  name:@"BackgroundChanged"
+                                               object:nil];
+                                             
+    // Listen for navigation changes to ensure transparency
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(navigationControllerDidShow:)
+                                                 name:@"UINavigationControllerDidShowViewControllerNotification"
                                                object:nil];
 }
 
@@ -47,26 +55,50 @@ extern NSMutableDictionary *prefDict;
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+- (void)navigationControllerDidShow:(NSNotification *)notification {
+    // Ensure transparency is maintained when navigating
+    if ([[BackgroundManager sharedManager] hasBackground]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[BackgroundManager sharedManager] makeSplitViewControllerTransparent:self];
+        });
+    }
+}
+
 - (void)applyBackground {
-    [[BackgroundManager sharedManager] applyBackgroundToView:self.view];
+    [[BackgroundManager sharedManager] applyBackgroundToSplitViewController:self];
 }
 
 - (void)backgroundChanged:(NSNotification *)notification {
-    // Remove existing background and reapply
-    [[BackgroundManager sharedManager] removeBackgroundFromView:self.view];
-    [self applyBackground];
+    // Reapply background when changed
+    [[BackgroundManager sharedManager] applyBackgroundToSplitViewController:self];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    // Resume video background if needed
+    
+    // Resume video background
     [[BackgroundManager sharedManager] resumeVideo];
+    
+    // Ensure transparency
+    if ([[BackgroundManager sharedManager] hasBackground]) {
+        [[BackgroundManager sharedManager] makeSplitViewControllerTransparent:self];
+    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    // Pause video background to save resources
+    // Pause video to save resources
     [[BackgroundManager sharedManager] pauseVideo];
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    [self changeDisplayModeForSize:size];
+    
+    // Update background frame on rotation
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        [[BackgroundManager sharedManager] updateBackgroundFrame];
+    } completion:nil];
 }
 
 - (void)splitViewController:(UISplitViewController *)svc willChangeToDisplayMode:(UISplitViewControllerDisplayMode)displayMode {
@@ -75,12 +107,6 @@ extern NSMutableDictionary *prefDict;
             self.preferredDisplayMode = UISplitViewControllerDisplayModeSecondaryOnly;
         });
     }
-}
-
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
-{
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-    [self changeDisplayModeForSize:size];
 }
 
 - (void)changeDisplayModeForSize:(CGSize)size {

fb9613d4fdfc21bd5ed44746728731ebcc074505
yitenchen123 <2916782430@qq.com>
2026-02-23 14:26:03 +0800

    创建 ProfileSettingsViewController.h

---
+++ Natives/ProfileSettingsViewController.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+// 版本设置 - 管理当前版本的模组/光影/Java/内存/渲染器
+@interface ProfileSettingsViewController : UITableViewController
+
+@property (nonatomic, strong) NSString *profileName;
+
+@end

64b14518e03db98780c9bebbe0943fafa668a51f
yitenchen123 <2916782430@qq.com>
2026-02-23 09:31:54 +0800

    创建 LauncherRootViewController.m

---
+++ Natives/LauncherRootViewController.m
@@ -0,0 +1,195 @@
+#import "LauncherRootViewController.h"
+#import "LauncherMenuViewController.h"
+#import "LauncherNewsViewController.h"
+#import "LauncherRightPanelViewController.h"
+#import "BackgroundManager.h"
+
+// 布局常量
+static const CGFloat kSidebarWidth = 70.0;      // 左侧边栏宽度
+static const CGFloat kRightPanelWidth = 220.0;  // 右侧面板宽度
+
+@interface LauncherRootViewController ()
+
+@property(nonatomic, strong) UIView *sidebarContainer;
+@property(nonatomic, strong) UIView *contentContainer;
+@property(nonatomic, strong) UIView *rightPanelContainer;
+
+@property(nonatomic, strong) NSLayoutConstraint *contentLeadingConstraint;
+@property(nonatomic, strong) NSLayoutConstraint *contentTrailingConstraint;
+
+@end
+
+@implementation LauncherRootViewController
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor clearColor];
+    
+    // 创建三个容器视图
+    [self setupContainers];
+    
+    // 添加子视图控制器
+    [self setupChildViewControllers];
+    
+    // 应用背景
+    [[BackgroundManager sharedManager] applyBackgroundToView:self.view];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[BackgroundManager sharedManager] resumeVideo];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[BackgroundManager sharedManager] pauseVideo];
+}
+
+#pragma mark - Setup
+
+- (void)setupContainers {
+    // 左侧边栏容器
+    self.sidebarContainer = [[UIView alloc] init];
+    self.sidebarContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    self.sidebarContainer.backgroundColor = [UIColor colorWithWhite:0.08 alpha:0.95];
+    [self.view addSubview:self.sidebarContainer];
+    
+    // 中间内容容器
+    self.contentContainer = [[UIView alloc] init];
+    self.contentContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    self.contentContainer.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.contentContainer];
+    
+    // 右侧面板容器
+    self.rightPanelContainer = [[UIView alloc] init];
+    self.rightPanelContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    self.rightPanelContainer.backgroundColor = [UIColor colorWithWhite:0.08 alpha:0.95];
+    [self.view addSubview:self.rightPanelContainer];
+    
+    // 设置约束
+    [NSLayoutConstraint activateConstraints:@[
+        // 左侧边栏
+        [self.sidebarContainer.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
+        [self.sidebarContainer.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+        [self.sidebarContainer.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+        [self.sidebarContainer.widthAnchor constraintEqualToConstant:kSidebarWidth],
+        
+        // 右侧面板
+        [self.rightPanelContainer.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
+        [self.rightPanelContainer.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+        [self.rightPanelContainer.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+        [self.rightPanelContainer.widthAnchor constraintEqualToConstant:kRightPanelWidth],
+        
+        // 中间内容区
+        [self.contentContainer.leadingAnchor constraintEqualToAnchor:self.sidebarContainer.trailingAnchor],
+        [self.contentContainer.trailingAnchor constraintEqualToAnchor:self.rightPanelContainer.leadingAnchor],
+        [self.contentContainer.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+        [self.contentContainer.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
+    ]];
+}
+
+- (void)setupChildViewControllers {
+    // 左侧边栏 - 功能菜单
+    LauncherMenuViewController *sidebarVC = [[LauncherMenuViewController alloc] init];
+    [self addChildViewController:sidebarVC];
+    sidebarVC.view.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.sidebarContainer addSubview:sidebarVC.view];
+    [NSLayoutConstraint activateConstraints:@[
+        [sidebarVC.view.leadingAnchor constraintEqualToAnchor:self.sidebarContainer.leadingAnchor],
+        [sidebarVC.view.trailingAnchor constraintEqualToAnchor:self.sidebarContainer.trailingAnchor],
+        [sidebarVC.view.topAnchor constraintEqualToAnchor:self.sidebarContainer.topAnchor],
+        [sidebarVC.view.bottomAnchor constraintEqualToAnchor:self.sidebarContainer.bottomAnchor]
+    ]];
+    [sidebarVC didMoveToParentViewController:self];
+    _sidebarViewController = sidebarVC;
+    
+    // 中间内容 - 默认显示新闻页
+    LauncherNewsViewController *newsVC = [[LauncherNewsViewController alloc] init];
+    [self setContentViewController:newsVC animated:NO];
+    
+    // 右侧面板 - 账户和启动
+    LauncherRightPanelViewController *rightPanelVC = [[LauncherRightPanelViewController alloc] init];
+    [self addChildViewController:rightPanelVC];
+    rightPanelVC.view.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.rightPanelContainer addSubview:rightPanelVC.view];
+    [NSLayoutConstraint activateConstraints:@[
+        [rightPanelVC.view.leadingAnchor constraintEqualToAnchor:self.rightPanelContainer.leadingAnchor],
+        [rightPanelVC.view.trailingAnchor constraintEqualToAnchor:self.rightPanelContainer.trailingAnchor],
+        [rightPanelVC.view.topAnchor constraintEqualToAnchor:self.rightPanelContainer.topAnchor],
+        [rightPanelVC.view.bottomAnchor constraintEqualToAnchor:self.rightPanelContainer.bottomAnchor]
+    ]];
+    [rightPanelVC didMoveToParentViewController:self];
+    _rightPanelViewController = rightPanelVC;
+}
+
+#pragma mark - Content Switching
+
+- (void)setContentViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    if (!viewController) return;
+    
+    UIViewController *oldVC = _contentViewController;
+    
+    // 移除旧的
+    if (oldVC) {
+        if (animated) {
+            [UIView transitionWithView:self.contentContainer
+                              duration:0.25
+                               options:UIViewAnimationOptionTransitionCrossDissolve
+                            animations:^{
+                                [oldVC willMoveToParentViewController:nil];
+                                [oldVC.view removeFromSuperview];
+                                [oldVC removeFromParentViewController];
+                            } completion:nil];
+        } else {
+            [oldVC willMoveToParentViewController:nil];
+            [oldVC.view removeFromSuperview];
+            [oldVC removeFromParentViewController];
+        }
+    }
+    
+    // 添加新的
+    _contentViewController = viewController;
+    [self addChildViewController:viewController];
+    viewController.view.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    if (animated && oldVC) {
+        [UIView transitionWithView:self.contentContainer
+                          duration:0.25
+                           options:UIViewAnimationOptionTransitionCrossDissolve
+                        animations:^{
+                            [self.contentContainer addSubview:viewController.view];
+                            [NSLayoutConstraint activateConstraints:@[
+                                [viewController.view.leadingAnchor constraintEqualToAnchor:self.contentContainer.leadingAnchor],
+                                [viewController.view.trailingAnchor constraintEqualToAnchor:self.contentContainer.trailingAnchor],
+                                [viewController.view.topAnchor constraintEqualToAnchor:self.contentContainer.topAnchor],
+                                [viewController.view.bottomAnchor constraintEqualToAnchor:self.contentContainer.bottomAnchor]
+                            ]];
+                        } completion:^(BOOL finished) {
+                            [viewController didMoveToParentViewController:self];
+                        }];
+    } else {
+        [self.contentContainer addSubview:viewController.view];
+        [NSLayoutConstraint activateConstraints:@[
+            [viewController.view.leadingAnchor constraintEqualToAnchor:self.contentContainer.leadingAnchor],
+            [viewController.view.trailingAnchor constraintEqualToAnchor:self.contentContainer.trailingAnchor],
+            [viewController.view.topAnchor constraintEqualToAnchor:self.contentContainer.topAnchor],
+            [viewController.view.bottomAnchor constraintEqualToAnchor:self.contentContainer.bottomAnchor]
+        ]];
+        [viewController didMoveToParentViewController:self];
+    }
+}
+
+#pragma mark - Orientation
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+@end

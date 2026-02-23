b898b27657e470feea3495b5b10139d03a0b8390
yitenchen123 <2916782430@qq.com>
2026-02-23 10:11:45 +0800

    更新 SceneDelegate.m

--- Natives/SceneDelegate.m
+++ Natives/SceneDelegate.m
@@ -1,7 +1,7 @@
 #import "SceneDelegate.h"
 #import "ios_uikit_bridge.h"
 #import "utils.h"
-#import "LauncherSplitViewController.h"
+#import "LauncherRootViewController.h"
 
 extern UIWindow *mainWindow;
 
@@ -13,7 +13,7 @@ extern UIWindow *mainWindow;
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
     UIWindowScene *windowScene = (UIWindowScene *)scene;
     
-    // Force landscape orientation for iOS 16+
+    // 强制横屏 (iOS 16+)
     if (@available(iOS 16.0, *)) {
         UIWindowSceneGeometryPreferencesIOS *geometryPreferences = [[UIWindowSceneGeometryPreferencesIOS alloc] init];
         geometryPreferences.interfaceOrientations = UIInterfaceOrientationMaskLandscape;
@@ -26,48 +26,32 @@ extern UIWindow *mainWindow;
     self.window.frame = windowScene.coordinateSpace.bounds;
     mainWindow = self.window;
     
-    // Use FCL-style split view controller
-    [self setupFCLInterface];
+    // 使用 FCL 风格根视图控制器（三栏布局）
+    LauncherRootViewController *rootVC = [[LauncherRootViewController alloc] init];
+    self.window.rootViewController = rootVC;
     
     [self.window makeKeyAndVisible];
 }
 
-- (void)setupFCLInterface {
-    // Create FCL-style split view controller
-    LauncherSplitViewController *splitVC = [[LauncherSplitViewController alloc] init];
-    
-    // Configure split view for FCL style
-    splitVC.preferredDisplayMode = UISplitViewControllerDisplayModeOneBesideSecondary;
-    splitVC.preferredSplitBehavior = UISplitViewControllerSplitBehaviorTile;
-    
-    self.window.rootViewController = splitVC;
-}
-
 - (void)sceneDidDisconnect:(UIScene *)scene {
-    // Called as the scene is being released by the system.
 }
 
 - (void)sceneDidBecomeActive:(UIScene *)scene {
-    // Called when the scene has moved from an inactive state to an active state.
 }
 
 - (void)sceneWillResignActive:(UIScene *)scene {
-    // Called when the scene will move from an active state to an inactive state.
 }
 
 - (void)sceneWillEnterForeground:(UIScene *)scene {
-    // Called as the scene transitions from the background to the foreground.
 }
 
 - (void)sceneDidEnterBackground:(UIScene *)scene {
-    // Called as the scene transitions from the foreground to the background.
     CallbackBridge_pauseGameIfNeed();
 }
 
 #pragma mark - Orientation Support (iOS 16+)
 
 - (UIInterfaceOrientationMask)scene:(UIScene *)scene supportedInterfaceOrientationsForWindowScene:(UIWindowScene *)windowScene API_AVAILABLE(ios(16.0)) {
-    // Force landscape only
     return UIInterfaceOrientationMaskLandscape;
 }
 

409d04513f2775277b947137b4e97d4b174d5fac
yitenchen123 <2916782430@qq.com>
2026-02-24 10:43:07 +0800

    更新 ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -7,9 +7,24 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ModrinthAPI : ModpackAPI <NSURLSessionDelegate>
 + (instancetype)sharedInstance;
-- (void)getVersionsForModWithID:(NSString *)modID completion:(void (^)(NSArray<ModVersion *> * _Nullable versions, NSError * _Nullable error))completion;
-- (void)searchShaderWithFilters:(NSDictionary *)filters completion:(void (^)(NSArray * _Nullable results, NSError * _Nullable error))completion;
-- (void)getVersionsForShaderWithID:(NSString *)shaderID completion:(void (^)(NSArray<ShaderVersion *> * _Nullable versions, NSError * _Nullable error))completion;
+
+// 现有的同步方法（保留用于兼容）
+- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, NSString *> *)searchFilters 
+                      previousPageResult:(NSMutableArray *)modrinthSearchResult;
+
+// 新增：异步搜索方法（修复 DownloadViewController 的调用）
+- (void)searchModWithFilters:(NSDictionary *)filters 
+                  completion:(void (^)(NSArray * _Nullable results, NSError * _Nullable error))completion;
+
+- (void)getVersionsForModWithID:(NSString *)modID 
+                     completion:(void (^)(NSArray<ModVersion *> * _Nullable versions, NSError * _Nullable error))completion;
+
+- (void)searchShaderWithFilters:(NSDictionary *)filters 
+                     completion:(void (^)(NSArray * _Nullable results, NSError * _Nullable error))completion;
+
+- (void)getVersionsForShaderWithID:(NSString *)shaderID 
+                        completion:(void (^)(NSArray<ShaderVersion *> * _Nullable versions, NSError * _Nullable error))completion;
+
 @end
 
 NS_ASSUME_NONNULL_END

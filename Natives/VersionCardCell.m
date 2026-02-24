d05f4ccefaf02fce91cf49fb58547879968585f8
yitenchen123 <2916782430@qq.com>
2026-02-24 13:03:06 +0800

    更新 VersionCardCell.m

--- Natives/VersionCardCell.m
+++ Natives/VersionCardCell.m
@@ -3,106 +3,98 @@
 
 @implementation VersionCardCell
 
-- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
     if (self) {
-        [self setupUI];
+        self.backgroundColor = [UIColor colorWithWhite:0.15 alpha:0.8];
+        self.layer.cornerRadius = 12;
+        self.layer.masksToBounds = YES;
+        
+        // 图标
+        self.iconImageView = [[UIImageView alloc] init];
+        self.iconImageView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.iconImageView.contentMode = UIViewContentModeScaleAspectFit;
+        self.iconImageView.image = [UIImage systemImageNamed:@"cube.fill"];
+        self.iconImageView.tintColor = [UIColor systemGreenColor];
+        [self.contentView addSubview:self.iconImageView];
+        
+        // 版本号
+        self.versionLabel = [[UILabel alloc] init];
+        self.versionLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        self.versionLabel.font = [UIFont boldSystemFontOfSize:14];
+        self.versionLabel.textAlignment = NSTextAlignmentCenter;
+        self.versionLabel.textColor = [UIColor labelColor];
+        [self.contentView addSubview:self.versionLabel];
+        
+        // 日期
+        self.dateLabel = [[UILabel alloc] init];
+        self.dateLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        self.dateLabel.font = [UIFont systemFontOfSize:10];
+        self.dateLabel.textColor = [UIColor secondaryLabelColor];
+        self.dateLabel.textAlignment = NSTextAlignmentCenter;
+        [self.contentView addSubview:self.dateLabel];
+        
+        // 类型标签
+        self.typeLabel = [[UILabel alloc] init];
+        self.typeLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        self.typeLabel.font = [UIFont systemFontOfSize:9];
+        self.typeLabel.textColor = [UIColor whiteColor];
+        self.typeLabel.backgroundColor = [UIColor systemBlueColor];
+        self.typeLabel.textAlignment = NSTextAlignmentCenter;
+        self.typeLabel.layer.cornerRadius = 4;
+        self.typeLabel.layer.masksToBounds = YES;
+        [self.contentView addSubview:self.typeLabel];
+        
+        [NSLayoutConstraint activateConstraints:@[
+            [self.iconImageView.topAnchor constraintEqualToAnchor:self.contentView.topAnchor constant:10],
+            [self.iconImageView.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+            [self.iconImageView.widthAnchor constraintEqualToConstant:40],
+            [self.iconImageView.heightAnchor constraintEqualToConstant:40],
+            
+            [self.versionLabel.topAnchor constraintEqualToAnchor:self.iconImageView.bottomAnchor constant:6],
+            [self.versionLabel.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:4],
+            [self.versionLabel.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-4],
+            
+            [self.dateLabel.topAnchor constraintEqualToAnchor:self.versionLabel.bottomAnchor constant:2],
+            [self.dateLabel.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:4],
+            [self.dateLabel.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-4],
+            
+            [self.typeLabel.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor constant:-6],
+            [self.typeLabel.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+            [self.typeLabel.widthAnchor constraintEqualToConstant:45],
+            [self.typeLabel.heightAnchor constraintEqualToConstant:16]
+        ]];
     }
     return self;
 }
 
-- (void)setupUI {
-    // 图标
-    _iconImageView = [[UIImageView alloc] init];
-    _iconImageView.translatesAutoresizingMaskIntoConstraints = NO;
-    _iconImageView.contentMode = UIViewContentModeScaleAspectFit;
-    [self.contentView addSubview:_iconImageView];
-    
-    // 版本标签
-    _versionLabel = [[UILabel alloc] init];
-    _versionLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    _versionLabel.font = [UIFont boldSystemFontOfSize:16];
-    _versionLabel.textColor = [UIColor labelColor];
-    [self.contentView addSubview:_versionLabel];
-    
-    // 类型标签
-    _typeLabel = [[UILabel alloc] init];
-    _typeLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    _typeLabel.font = [UIFont systemFontOfSize:12];
-    _typeLabel.textColor = [UIColor secondaryLabelColor];
-    _typeLabel.layer.cornerRadius = 4;
-    _typeLabel.layer.masksToBounds = YES;
-    [self.contentView addSubview:_typeLabel];
-    
-    // 日期标签
-    _dateLabel = [[UILabel alloc] init];
-    _dateLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    _dateLabel.font = [UIFont systemFontOfSize:12];
-    _dateLabel.textColor = [UIColor tertiaryLabelColor];
-    [self.contentView addSubview:_dateLabel];
-    
-    [self setupConstraints];
-}
-
-- (void)setupConstraints {
-    CGFloat margin = 12;
-    
-    [NSLayoutConstraint activateConstraints:@[
-        // 图标
-        [_iconImageView.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:margin],
-        [_iconImageView.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor],
-        [_iconImageView.widthAnchor constraintEqualToConstant:40],
-        [_iconImageView.heightAnchor constraintEqualToConstant:40],
-        
-        // 版本标签
-        [_versionLabel.leadingAnchor constraintEqualToAnchor:_iconImageView.trailingAnchor constant:margin],
-        [_versionLabel.topAnchor constraintEqualToAnchor:self.contentView.topAnchor constant:margin],
-        [_versionLabel.trailingAnchor constraintLessThanOrEqualToAnchor:self.contentView.trailingAnchor constant:-margin],
-        
-        // 类型标签
-        [_typeLabel.leadingAnchor constraintEqualToAnchor:_versionLabel.leadingAnchor],
-        [_typeLabel.topAnchor constraintEqualToAnchor:_versionLabel.bottomAnchor constant:4],
-        [_typeLabel.heightAnchor constraintEqualToConstant:18],
-        
-        // 日期标签
-        [_dateLabel.leadingAnchor constraintEqualToAnchor:_typeLabel.trailingAnchor constant:8],
-        [_dateLabel.centerYAnchor constraintEqualToAnchor:_typeLabel.centerYAnchor],
-        [_dateLabel.trailingAnchor constraintLessThanOrEqualToAnchor:self.contentView.trailingAnchor constant:-margin],
-        [_dateLabel.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor constant:-margin]
-    ]];
-}
-
-- (void)configureWithIcon:(UIImage *)icon
-                     date:(NSString *)date
-                     type:(NSString *)type
-                  version:(NSString *)version {
-    self.iconImageView.image = icon;
+- (void)configureWithVersionId:(NSString *)versionId
+                          date:(NSString *)date
+                          type:(NSString *)type {
+    self.versionLabel.text = versionId;
     self.dateLabel.text = date;
     self.typeLabel.text = type;
-    self.versionLabel.text = version;
     
-    // 根据类型设置背景色
-    if ([type isEqualToString:@"Release"]) {
+    // 根据类型设置颜色
+    if ([type isEqualToString:@"正式版"] || [type isEqualToString:@"release"]) {
         self.typeLabel.backgroundColor = [UIColor systemGreenColor];
-        self.typeLabel.textColor = [UIColor whiteColor];
-    } else if ([type isEqualToString:@"Beta"]) {
+        self.typeLabel.text = @"正式版";
+    } else if ([type isEqualToString:@"测试版"] || [type isEqualToString:@"snapshot"]) {
         self.typeLabel.backgroundColor = [UIColor systemOrangeColor];
-        self.typeLabel.textColor = [UIColor whiteColor];
-    } else if ([type isEqualToString:@"Alpha"]) {
-        self.typeLabel.backgroundColor = [UIColor systemRedColor];
-        self.typeLabel.textColor = [UIColor whiteColor];
+        self.typeLabel.text = @"测试版";
     } else {
-        self.typeLabel.backgroundColor = [UIColor systemGrayColor];
-        self.typeLabel.textColor = [UIColor whiteColor];
+        self.typeLabel.backgroundColor = [UIColor systemPurpleColor];
+        self.typeLabel.text = @"远古版";
     }
 }
 
 - (void)prepareForReuse {
     [super prepareForReuse];
-    self.iconImageView.image = nil;
+    self.iconImageView.image = [UIImage systemImageNamed:@"cube.fill"];
+    self.versionLabel.text = nil;
     self.dateLabel.text = nil;
     self.typeLabel.text = nil;
-    self.versionLabel.text = nil;
+    self.typeLabel.backgroundColor = [UIColor systemBlueColor];
 }
 
 @end

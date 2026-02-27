c00573e2168bd2f5ad04bc0862862db03e3f90f6
yitenchen123 <2916782430@qq.com>
2026-02-27 13:00:17 +0800

    添加缺失的属性

--- Natives/installer/FabricInstallViewController.h
+++ Natives/installer/FabricInstallViewController.h
@@ -2,4 +2,9 @@
 #import "PLPrefTableViewController.h"
 
 @interface FabricInstallViewController : PLPrefTableViewController
+
+@property (nonatomic, copy) NSString *gameVersion;
+@property (nonatomic, assign) BOOL shouldInstallAPI;
+@property (nonatomic, copy) void (^completionHandler)(BOOL success, NSString *profileName, NSError *error);
+
 @end

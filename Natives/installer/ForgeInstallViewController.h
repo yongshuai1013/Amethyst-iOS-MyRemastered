eda20db57a4b006c73df6a797c46bb0ec00c7d72
yitenchen123 <2916782430@qq.com>
2026-02-27 13:01:03 +0800

    添加缺失的属性

--- Natives/installer/ForgeInstallViewController.h
+++ Natives/installer/ForgeInstallViewController.h
@@ -1,4 +1,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ForgeInstallViewController : UITableViewController
+
+@property (nonatomic, copy) NSString *gameVersion;
+@property (nonatomic, assign) BOOL isNeoForge;
+@property (nonatomic, copy) void (^completionHandler)(BOOL success, NSString *profileName, NSError *error);
+
 @end
